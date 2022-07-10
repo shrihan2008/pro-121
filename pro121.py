4d1f130bb3f2e1c83507e04330538e4f8f160ab8
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-07-10 12:41:22 +0530

    Add files via upload

--- pro121.py
+++ pro121.py
@@ -3,13 +3,14 @@ import cv2
 import time
 from cv2 import COLOR_BGR2HSV
 import numpy as np
+cap=cv2.VideoCapture(0)
+Image=cap.read()
 
 fourcc=cv2.VideoWriter_fourcc(*"XVID")
-output_file=cv2.VideoWriter('output.avi',fourcc,15.0,(640,4800))
+Frame=cv2.VideoWriter('output.avi',fourcc,15.0,(1000,1000))
 
-
-cap=cv2.VideoCapture(0)
-time.sleep(2)
+Image=cv2.resize(Image,(640,480))
+Frame=cv2.resize(Frame,(640,480))
 bg=0
 for i in range(60):
     ret,bg=cap.read()
@@ -25,24 +26,22 @@ while(cap.isOpened()):
     u_black=np.array([104, 153,78])
     l_black=np.array([30, 30, 5])
     
-    mask_1=cv2.inRange(hsv,l_black,u_black)
+    mask=cv2.inRange(hsv,l_black,u_black)
+    res=cv2.bitwise_and(Frame,Frame,mask=mask)
 
-    l_black=np.array([104,153,78])
-    u_black=np.array([30, 30, 0])
-    mask_2=cv2.inRange(hsv,l_black,u_black)
-    mask_1=mask_1+mask_2
-
-    mask_1=cv2.morphologyEx(mask_1,cv2.MORPH_OPEN,np.ones((3,3),np.uint8))
-    mask_1=cv2.morphologyEx(mask_1,cv2.MORPH_DILATE,np.ones((3,3),np.uint8))
-
-    mask_2=cv2.bitwise_not(mask_1)
-    res_1=cv2.bitwise_and(img,img,mask=mask_2)
-    res_2=cv2.bitwise_and(bg,bg,mask=mask_1)
-
-    last_output=cv2.addWeighted(res_1,1,res_2,1,0)
-    output_file.write(last_output)
+    last_output=cv2.addWeighted(res,1,res,1,0)
+    Frame.write(last_output)
     cv2.imshow("Hello",last_output)
     cv2.waitKey(1)
+
+    
+    f=Frame-res
+    f=np.where(f==0,Image,f)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+            break
+
+
 
 cap.release()
 out.release()
